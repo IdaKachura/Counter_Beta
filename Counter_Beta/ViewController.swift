42939480c5f4cfe69882aef0e8d88108d000f279
IdaKachura <idakachura@yandex.ru>
2022-08-17 23:25:47 +0300

    В AppDelegate создан код для тестирования функции счёта

--- Counter_Beta/ViewController.swift
+++ Counter_Beta/ViewController.swift
@@ -2,18 +2,26 @@
 //  ViewController.swift
 //  Counter_Beta
 //
-//  Created by Владимир Качура on 17.08.2022.
+//  Created by Ида Качура on 07.08.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var accountNumberLabel: UILabel!
+    @IBOutlet weak var accountChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    var count: Int = 0
+    
+    @IBAction func accountChangeButton(_ sender: Any) {
+     
+            count += 1
+            accountNumberLabel.text = "Значение счётчика : \(count)"
+        
+        }
 }
 
